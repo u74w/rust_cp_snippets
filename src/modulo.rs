8e4c550ac155ce54542faec261892a27af5c4285
p232069y <p232069y@mail.kyutech.jp>
2018-12-20 04:37:34 +0900

    add prime_factor

--- src/modulo.rs
+++ src/modulo.rs
@@ -1,4 +1,4 @@
-#[snippet = "mod"]
+#[snippet = "gcd"]
 #[allow(dead_code)]
 pub fn gcd(a: u64, b: u64) -> u64 {
     if b == 0 {
@@ -8,13 +8,15 @@ pub fn gcd(a: u64, b: u64) -> u64 {
     }
 }
 
-#[snippet = "mod"]
+#[snippet = "lcm"]
+#[snippet(include = "gcd")]
 #[allow(dead_code)]
 pub fn lcm(a: u64, b: u64) -> u64 {
     a / gcd(a, b) * b
 }
 
-#[snippet = "mod"]
+#[snippet = "extgcd"]
+#[snippet(include = "gcd")]
 #[allow(dead_code)]
 pub fn extgcd(a: i64, b: i64) -> (i64, i64, i64) {
     if b == 0 {
@@ -52,8 +54,7 @@ pub fn prime(n: usize) -> Vec<bool> {
 pub fn divisor(n: u64) -> Vec<u64> {
     let mut div: Vec<u64> = Vec::with_capacity(n as usize);
     let mut i = 1;
-    loop {
-        if i * i > n { break; }
+    while i * i <= n {
         if n % i == 0 {
             div.push(i);
             if i * i != n {
@@ -66,6 +67,28 @@ pub fn divisor(n: u64) -> Vec<u64> {
     return div;
 }
 
+use std::collections::HashMap;
+#[snippet = "prime_factor"]
+#[allow(dead_code)]
+pub fn prime_factor(mut n: u64) -> HashMap<u64, u64> {
+    let mut prime_factors: HashMap<u64, u64> = HashMap::with_capacity(n as usize);
+    let mut i = 2;
+    while i * i <= n {
+        while n % i == 0 {
+            if prime_factors.contains_key(&i) {
+                prime_factors.insert(i, prime_factors[&i]+1);
+            } else {
+                prime_factors.insert(i, 1);
+            }
+            n /= i;
+        }
+        i += 1;
+    }
+    if n != 1 {
+        prime_factors.insert(n, 1);
+    }
+    return prime_factors;
+}
 
 #[test]
 fn test_lcm() {
@@ -81,4 +104,11 @@ fn test_prime() {
 #[test]
 fn test_divisor() {
     assert_eq!(divisor(10), vec![1, 2, 5, 10]);
+}
+
+#[test]
+fn test_prime_factor() {
+    let mut hoge: HashMap<u64, u64> = HashMap::new();
+    hoge.insert(2, 3);
+    assert_eq!(prime_factor(8), hoge);
 }
