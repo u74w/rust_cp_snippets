f04254d1f4b67572ec68958d735bf9f55b8235a8
p232069y <p232069y@mail.kyutech.jp>
2019-02-03 20:31:44 +0900

    add comb

--- src/modulo.rs
+++ src/modulo.rs
@@ -1,8 +1,8 @@
-#[snippet = "MOD"]
+#[snippet = "mod"]
 #[allow(dead_code)]
 pub const MOD: u64 = 1_000_000_007;
 
-#[snippet = "gcd"]
+#[snippet = "mod"]
 #[allow(dead_code)]
 pub fn gcd(a: u64, b: u64) -> u64 {
     if b == 0 {
@@ -12,15 +12,13 @@ pub fn gcd(a: u64, b: u64) -> u64 {
     }
 }
 
-#[snippet = "lcm"]
-#[snippet(include = "gcd")]
+#[snippet = "mod"]
 #[allow(dead_code)]
 pub fn lcm(a: u64, b: u64) -> u64 {
     a / gcd(a, b) * b
 }
 
-#[snippet = "extgcd"]
-#[snippet(include = "gcd")]
+#[snippet = "mod"]
 #[allow(dead_code)]
 pub fn extgcd(a: i64, b: i64) -> (i64, i64, i64) {
     if b == 0 {
@@ -78,12 +76,7 @@ pub fn prime_factor(n: u64) -> HashMap<u64, u64> {
     let mut i = 2;
     while i * i <= n {
         while n % i == 0 {
-            if prime_factors.contains_key(&i) {
-                let exponent = prime_factors[&i] + 1;
-                prime_factors.insert(i, exponent);
-            } else {
-                prime_factors.insert(i, 1);
-            }
+            *prime_factors.entry(i).or_insert(0) += 1;
             n /= i;
         }
         i += 1;
@@ -94,7 +87,7 @@ pub fn prime_factor(n: u64) -> HashMap<u64, u64> {
     prime_factors
 }
 
-#[snippet = "mod_pow"]
+#[snippet = "mod"]
 #[allow(dead_code)]
 pub fn mod_pow(x: u64, n: u64, m: u64) -> u64 {
     let mut res = 1;
@@ -110,11 +103,85 @@ pub fn mod_pow(x: u64, n: u64, m: u64) -> u64 {
     res
 }
 
-#[snippet = "mod_inv"]
+#[snippet = "mod"]
 #[allow(dead_code)]
 pub fn mod_inv(a: u64, m: u64) -> u64 {
     let (_, x, _) = extgcd(a as i64, m as i64);
     ((m as i64 + x) as u64 % m) % m
+}
+
+#[snippet = "mod"]
+#[allow(dead_code)]
+pub fn fact_table(size: usize, m: u64) -> Vec<u64> {
+    let mut table = vec![1; size + 1];
+    for i in 1..size + 1 {
+        table[i] = (i as u64 * table[i - 1]) % m;
+    }
+    table
+}
+
+#[snippet = "mod"]
+#[allow(dead_code)]
+/// Factorial and Inverse factorial table
+pub fn fact_inv_table(size: usize, m: u64) -> (Vec<u64>, Vec<u64>) {
+    let mut fact = vec![1; size];
+    let mut fact_inv = vec![1; size];
+
+    for i in 2..size {
+        fact[i] = fact[i - 1] * i as u64 % m;
+        fact_inv[i] = m - ((m / i as u64) * fact_inv[(m % i as u64) as usize] % m);
+    }
+
+    for i in 1..size {
+        fact_inv[i] = fact_inv[i - 1] * fact_inv[i] % m;
+    }
+
+    (fact, fact_inv)
+}
+
+#[snippet = "mod"]
+#[allow(dead_code)]
+#[snippet(include = "fact_table")]
+/// (a mod p, e when n! = a p\^e)
+pub fn mod_fact(n: u64, p: u64, fact: &[u64]) -> (u64, u64) {
+    if n == 0 {
+        (1, 0)
+    } else {
+        let (a, b) = mod_fact(n / p, p, fact);
+        let pow = b + n / p;
+
+        if n / p % 2 != 0 {
+            (a * (p - fact[(n % p) as usize]) % p, pow)
+        } else {
+            (a * fact[(n % p) as usize] % p, pow)
+        }
+    }
+}
+
+#[snippet = "mod"]
+#[allow(dead_code)]
+/// C(n, k) % p
+pub fn mod_comb(n: u64, k: u64, p: u64, fact: &[u64]) -> u64 {
+    if n < k {
+        0
+    } else {
+        let (a1, e1) = mod_fact(n, p, fact);
+        let (a2, e2) = mod_fact(k, p, fact);
+        let (a3, e3) = mod_fact(n - k, p, fact);
+
+        if e1 > e2 + e3 {
+            0
+        } else {
+            a1 * mod_inv(a2 * a3 % p, p) % p
+        }
+    }
+}
+
+#[snippet = "mod"]
+#[allow(dead_code)]
+/// H(n, k) % p
+pub fn mod_comb_repetition(n: u64, k: u64, p: u64, fact: &[u64]) -> u64 {
+    mod_comb(n - 1 + k, n - 1, p, fact)
 }
 
 #[test]
@@ -148,4 +215,27 @@ fn test_mod_pow() {
     assert_eq!(mod_pow(2, 3, MOD), 8);
     assert_eq!(mod_pow(2, 3, 6), 2);
     assert_eq!(mod_pow(2, 10, MOD), 1024);
+}
+
+#[test]
+fn test_fact_inv_table() {
+    let m = 1_000_000_007;
+    let size = 1000;
+    let (fact, fact_inv) = fact_inv_table(1000, m);
+
+    for i in 0..size {
+        assert_eq!(fact[i] * fact_inv[i] % m, 1);
+    }
+}
+
+#[test]
+fn test_mod_comb_repetition() {
+    let m = 1_000_000_007;
+    let fact = fact_table(200000, m);
+
+    assert_eq!(mod_comb_repetition(10, 2, m, &fact), 55);
+    assert_eq!(mod_comb_repetition(10, 3, m, &fact), 220);
+    assert_eq!(mod_comb_repetition(10, 4, m, &fact), 715);
+    assert_eq!(mod_comb_repetition(400, 296, m, &fact), 546898535);
+    assert_eq!(mod_comb_repetition(100000, 100000, m, &fact), 939733670);
 }
